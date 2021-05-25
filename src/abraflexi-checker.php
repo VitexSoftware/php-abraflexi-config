8077202ffee9dcd7d333b26f2681b8acbc8e5be0
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-05-25 12:16:49 +0200

    Package renamed to abraflexi-client-config

--- src/abraflexi-checker.php
+++ src/abraflexi-checker.php
@@ -8,15 +8,22 @@ define('EASE_LOGGER', 'console|syslog');
 
 $shared = new \Ease\Shared();
 
-if (\Ease\Functions::cfg('ABRAFLEXI_URL') === false) {
+$status = [];
+
+if (empty(\Ease\Functions::cfg('ABRAFLEXI_URL'))) {
     $cfgFile = '../client.json';
-    if(file_exists($cfgFile)){
-       $loaded = $shared->loadConfig($cfgFile, true);
-        \Ease\Shared::logger()->addStatusMessage('Loading config file '.$cfgFile, empty($loaded) ? 'warning' : 'success' );
+    if (file_exists($cfgFile)) {
+        $loaded = $shared->loadConfig($cfgFile, true);
+        $status[empty($loaded) ? 'warning' : 'success'] = 'Loading config file ' . $cfgFile . ' ' . count($loaded) . ' items loaded';
     }
+} else {
+    $status['info'] = 'loading configuration from environment';
 }
 
 $checker = new Company();
+
+$checker->addStatusMessage(current($status), key($status));
+
 $infoRaw = $checker->getFlexiData();
 $info = is_array($infoRaw) && !array_key_exists('message', $infoRaw) ? \Ease\Functions::reindexArrayBy($infoRaw, 'dbNazev') : [];
 $myCompany = $checker->getCompany();
