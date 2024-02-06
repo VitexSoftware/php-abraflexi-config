fe3a976b7a898a16c3a78757d38004e756b6831b
CyberVitexus <info@vitexsoftware.cz>
2024-02-06 15:09:58 +0100

    code cleanup

--- src/abraflexi-checker.php
+++ src/abraflexi-checker.php
@@ -3,17 +3,11 @@
 namespace AbraFlexi;
 
 require_once '../vendor/autoload.php';
-define('EASE_APPNAME', 'php-abraflexi-checker');
+define('EASE_APPNAME', 'AbraFlexiConnectionChecker');
 
-$shared = new \Ease\Shared();
-
-$status = [];
-
-\Ease\Shared::init(['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY'], file_exists('../.env') ? '../.env' : null);
+\Ease\Shared::init(['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY'], '../.env');
 
 $checker = new Company();
-
-$checker->addStatusMessage(current($status), key($status));
 
 $infoRaw = $checker->getFlexiData();
 $info = is_array($infoRaw) && !array_key_exists('message', $infoRaw) ? \Ease\Functions::reindexArrayBy($infoRaw, 'dbNazev') : [];
