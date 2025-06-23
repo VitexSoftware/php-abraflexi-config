46eb3c3eb30ff749d3d6d893d9c58c6bfb2604d6
CyberVitexus <info@vitexsoftware.cz>
2025-06-24 00:03:28 +0200

    coding standart psr12

--- src/abraflexi-checker.php
+++ src/abraflexi-checker.php
@@ -1,22 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of the EaseTWBootstrap4 package
+ *
+ * https://github.com/VitexSoftware/php-abraflexi-config
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AbraFlexi;
 
 require_once '../vendor/autoload.php';
-define('EASE_APPNAME', 'AbraFlexiConnectionChecker');
+\define('EASE_APPNAME', 'AbraFlexiConnectionChecker');
 
 \Ease\Shared::init(['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY'], '../.env');
 
 $checker = new Company();
 
 $infoRaw = $checker->getFlexiData();
-$info = is_array($infoRaw) && !array_key_exists('message', $infoRaw) ? \Ease\Functions::reindexArrayBy($infoRaw, 'dbNazev') : [];
+$info = \is_array($infoRaw) && !\array_key_exists('message', $infoRaw) ? \Ease\Functions::reindexArrayBy($infoRaw, 'dbNazev') : [];
 $myCompany = $checker->getCompany();
 $checker->logBanner();
 
 $checker->addStatusMessage(
     'connection test',
-    array_key_exists($myCompany, $info) ? 'success' : 'error'
+    \array_key_exists($myCompany, $info) ? 'success' : 'error',
 );
 
-return( array_key_exists($myCompany, $info) ? 0 : 1 );
+return \array_key_exists($myCompany, $info) ? 0 : 1;
--- src/abraflexi-config.php
+++ src/abraflexi-config.php
@@ -1,12 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of the EaseTWBootstrap4 package
+ *
+ * https://github.com/VitexSoftware/php-abraflexi-config
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once '../vendor/autoload.php';
-define('EASE_APPNAME', 'php-abraflexi-config');
+\define('EASE_APPNAME', 'php-abraflexi-config');
 
 $configFile = '../client.json';
-$exitcode   = 0;
+$exitcode = 0;
 
 $shared = new \Ease\Shared();
+
 if (file_exists($configFile)) {
     $shared->loadConfig($configFile, true);
 }
@@ -14,29 +28,35 @@ if (file_exists($configFile)) {
 switch ($argc) {
     case 1:
         foreach ($shared->configuration as $configKey => $configValue) {
-            echo sprintf("%-20s%s\n", $configKey . ':', $configValue);
+            echo sprintf("%-20s%s\n", $configKey.':', $configValue);
         }
+
         break;
     case 2:
-        if (array_key_exists($argv[1], $shared->configuration)) {
+        if (\array_key_exists($argv[1], $shared->configuration)) {
             echo $shared->getConfigValue($argv[1]);
         }
+
         break;
     case 3:
         $shared->setConfigValue($argv[1], $argv[2]);
         $result = file_put_contents(
             $configFile,
-            json_encode($shared->configuration, JSON_PRETTY_PRINT)
+            json_encode($shared->configuration, \JSON_PRETTY_PRINT),
         );
+
         if ($result === false) {
             $exitcode = 1;
         }
+
         break;
+
     default:
         echo 'Usage: ';
-        echo "       " . basename(__FILE__) . "                                   -  print current config";
-        echo "       " . basename(__FILE__) . " ABRAFLEXI_URL                     -  print given key's value";
-        echo "       " . basename(__FILE__) . " ABRAFLEXI_URL http://server:port  -  set given key's to value";
+        echo '       '.basename(__FILE__).'                                   -  print current config';
+        echo '       '.basename(__FILE__)." ABRAFLEXI_URL                     -  print given key's value";
+        echo '       '.basename(__FILE__)." ABRAFLEXI_URL http://server:port  -  set given key's to value";
+
         break;
 }
 
